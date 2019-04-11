88744d8c25a1c5e3105eed869b9d0f4120620066
Mike Ball <mikedball@gmail.com>
2019-04-11 07:07:33 -0400

    use more ES-y constructs and syntax

--- test/madison.js
+++ test/madison.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
-var madison = require('../lib/madison.js');
+const assert = require('assert');
+const madison = require('../lib/madison.js');
 
-describe('madison', function() {
-  describe('#getStateAbbrev', function () {
-    it('returns a state abbreviation when it is passed a state name', function () {
+describe('madison', () => {
+  describe('#getStateAbbrev', () => {
+    it('returns a state abbreviation when it is passed a state name', () => {
       madison.getStateAbbrev('virginia', function (abbrev) {
         assert(abbrev === 'VA');
       });
@@ -17,7 +17,7 @@ describe('madison', function() {
       });
     });
 
-    it('returns a state abbreviation when it is passed a state name, independent of case', function () {
+    it('returns a state abbreviation when it is passed a state name, independent of case', () => {
       madison.getStateAbbrev('Virginia', function (abbrev) {
         assert(abbrev === 'VA');
       });
@@ -31,7 +31,7 @@ describe('madison', function() {
       });
     });
 
-    it('returns undefined in the event that it is not passed a valid state name', function () {
+    it('returns undefined in the event that it is not passed a valid state name', () => {
       madison.getStateAbbrev('Some Fake State', function (abbrev) {
         assert(abbrev === undefined);
       });
@@ -41,7 +41,7 @@ describe('madison', function() {
       });
     });
 
-    it('returns undefined in the event that it is passed a non-string value', function () {
+    it('returns undefined in the event that it is passed a non-string value', () => {
       madison.getStateAbbrev(undefined, function(abbrev) {
         assert(abbrev === undefined);
       });
@@ -68,25 +68,25 @@ describe('madison', function() {
     });
   });
 
-  describe('#getStateAbbrevSync', function () {
-    it('returns a state abbreviation when it is passed a state name', function () {
+  describe('#getStateAbbrevSync', () => {
+    it('returns a state abbreviation when it is passed a state name', () => {
       assert(madison.getStateAbbrevSync('virginia') === 'VA');
       assert(madison.getStateAbbrevSync('west virginia') === 'WV');
       assert(madison.getStateAbbrevSync('California') === 'CA');
     });
 
-    it('returns a state abbreviation when it is passed a state name, independent of case', function () {
+    it('returns a state abbreviation when it is passed a state name, independent of case', () => {
       assert(madison.getStateAbbrevSync('Virginia') === 'VA');
       assert(madison.getStateAbbrevSync('West virginia') === 'WV');
       assert(madison.getStateAbbrevSync('californIA') === 'CA');
     });
 
-    it('returns undefined in the event that it is not passed a valid state name', function () {
+    it('returns undefined in the event that it is not passed a valid state name', () => {
       assert(madison.getStateAbbrevSync('Some Fake State') === undefined);
       assert(madison.getStateAbbrevSync('Another Fake State') === undefined);
     });
 
-    it('returns undefined in the event that it is passed a non-string value', function () {
+    it('returns undefined in the event that it is passed a non-string value', () => {
       assert(madison.getStateAbbrevSync(undefined) === undefined);
       assert(madison.getStateAbbrevSync(false) === undefined);
       assert(madison.getStateAbbrevSync(333) === undefined);
@@ -96,8 +96,8 @@ describe('madison', function() {
     });
   });
 
-  describe('#getStateName', function () {
-    it('returns a state name when it is passed a state abbreviation', function () {
+  describe('#getStateName', () => {
+    it('returns a state name when it is passed a state abbreviation', () => {
       madison.getStateName('va', function (name) {
         assert(name === 'Virginia');
       });
@@ -107,7 +107,7 @@ describe('madison', function() {
       });
     });
 
-    it('returns a state name when it is passed a state abbreviation, independent of case', function () {
+    it('returns a state name when it is passed a state abbreviation, independent of case', () => {
       madison.getStateName('VA', function (name) {
         assert(name === 'Virginia');
       });
@@ -117,7 +117,7 @@ describe('madison', function() {
       });
     });
 
-    it('returns undefined in the event that it is not passed a valid state abbreviation', function () {
+    it('returns undefined in the event that it is not passed a valid state abbreviation', () => {
       madison.getStateName('XX', function (name) {
         assert(name === undefined);
       });
@@ -127,7 +127,7 @@ describe('madison', function() {
       });
     });
 
-    it('returns undefined in the event that it is passed a non-string value', function () {
+    it('returns undefined in the event that it is passed a non-string value', () => {
       madison.getStateName(undefined, function (name) {
         assert(name === undefined);
       });
@@ -154,23 +154,23 @@ describe('madison', function() {
     });
   });
 
-  describe('#getStateNameSync', function () {
-    it('returns a state name when it is passed a state abbreviation', function () {
+  describe('#getStateNameSync', () => {
+    it('returns a state name when it is passed a state abbreviation', () => {
       assert(madison.getStateNameSync('va') === 'Virginia');
       assert(madison.getStateNameSync('ca') === 'California');
     });
 
-    it('returns a state name when it is passed a state abbreviation, independent of case', function () {
+    it('returns a state name when it is passed a state abbreviation, independent of case', () => {
       assert(madison.getStateNameSync('VA') === 'Virginia');
       assert(madison.getStateNameSync('cA') === 'California');
     });
 
-    it('returns undefined in the event that it is not passed a valid state abbreviation', function () {
+    it('returns undefined in the event that it is not passed a valid state abbreviation', () => {
       assert(madison.getStateNameSync('XX') === undefined);
       assert(madison.getStateNameSync('00') === undefined);
     });
 
-    it('returns undefined in the event that it is passed a non-string value', function () {
+    it('returns undefined in the event that it is passed a non-string value', () => {
       assert(madison.getStateNameSync(undefined) === undefined);
       assert(madison.getStateNameSync(false) === undefined);
       assert(madison.getStateNameSync(333) === undefined);
@@ -180,8 +180,8 @@ describe('madison', function() {
     });
   });
 
-  describe('#states', function () {
-    it('returns a JSON array of objects, each of which contains a `name` and `abbr` key', function () {
+  describe('#states', () => {
+    it('returns a JSON array of objects, each of which contains a `name` and `abbr` key', () => {
       assert(madison.states.length === 66);
       assert(madison.states[0].name === 'Alabama');
       assert(madison.states[0].abbr === 'AL');
